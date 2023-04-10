5f0975a0313bb790a417ad972d598926d964acd8
Nguyen Dinh Hong Phuc <contact.phuc1710@gmail.com>
2023-04-10 16:26:57 +0700

    feat: auto scroll

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:random_avatar/random_avatar.dart';
 
@@ -11,59 +12,65 @@ class ChatPage extends StatefulWidget {
 class _ChatPageState extends State<ChatPage> {
   final TextEditingController _textController = TextEditingController();
   List<String> messages = [];
+  final ScrollController _scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          title: const Text('Chat Page'),
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: messages.length,
-                shrinkWrap: true,
-                itemBuilder: (context, index) => Container(
-                  color: (index & 1 == 1) ? Colors.grey[200] : Colors.white,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(right: 8.0),
-                          child: RandomAvatar('lorem', width: 40),
-                        ),
-                        Expanded(
-                          child: Text(
-                            messages[index],
-                            overflow: TextOverflow.visible,
-                          ),
-                        ),
-                      ],
-                    ),
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(title: const Text('Chat Page')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: _scrollController,
+              itemCount: messages.length,
+              shrinkWrap: true,
+              itemBuilder: (context, index) => Container(
+                color: (index & 1 == 1) ? Colors.grey[200] : Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: RandomAvatar((index & 1 == 1) ? 'ChatNPT' : 'p',
+                            width: 40),
+                      ),
+                      Expanded(child: Text(messages[index])),
+                    ],
                   ),
                 ),
               ),
             ),
-            TextField(
-              controller: _textController,
-              decoration: InputDecoration(
-                border: const OutlineInputBorder(),
-                hintText: 'Input message',
-                suffixIcon: InkWell(
-                    onTap: () {
-                      if (_textController.text.isEmpty) return;
-                      setState(() => messages.add(_textController.text));
+          ),
+          TextField(
+            controller: _textController,
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              hintText: 'Input message',
+              suffixIcon: InkWell(
+                  onTap: () {
+                    if (_textController.text.isEmpty) return;
+                    setState(() {
+                      messages.add(_textController.text);
                       _textController.clear();
-                    },
-                    child: const Icon(Icons.send)),
-              ),
-              textInputAction: TextInputAction.send,
-            )
-          ],
-        ));
+                      WidgetsBinding.instance.addPostFrameCallback((_) {
+                        _scrollController.animateTo(
+                          _scrollController.position.maxScrollExtent,
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeOut,
+                        );
+                      });
+                    });
+                  },
+                  child: const Icon(Icons.send)),
+            ),
+            textInputAction: TextInputAction.send,
+          )
+        ],
+      ),
+    );
   }
 }
